1a4299fa1d3f518a9aaeca98e4e24a30685955bf
VirtualCaffeineIO <virtualcaffeineio@gmail.com>
2025-03-31 16:34:43 -0700

    Update Install.cmd

--- Intune/User_Scripts/UI_Settings/Install.cmd
+++ Intune/User_Scripts/UI_Settings/Install.cmd
@@ -1,8 +1,27 @@
-<#
-Wrapper script for Set-Win11Defaults.ps1
-.
-This is meant to be run at Autopilot to edit the defualt user windows 11 start menu UI settings
-#>
+:: ================================================
+:: Windows 11 UI Defaults Installer - Wrapper Script
+:: ================================================
+::
+:: This script executes the Set-Win11-Defaults.ps1 script using PowerShell
+:: and logs all output to the Intune Management Extension log directory.
+::
+:: Purpose:
+::   - Run a PowerShell script that applies Windows 11 UI tweaks
+::     for both the current user (HKCU) and the default profile (HKLM\TempUser).
+::
+:: Behavior:
+::   - Executes silently during ESP or Win32 app install
+::   - Captures stdout and stderr to:
+::       C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\Set-Win11Defaults.log
+::   - Returns the exit code from the PowerShell script to Intune
+::
+:: Notes:
+::   - Must be run as SYSTEM (i.e. Intune Win32 App, system context)
+::   - Logging is timestamped and stored with Intune logs for easy troubleshooting
+::
+:: Author: Virtual Caffeine IO
+:: Created: 3-31-2025
+:: WWW: virtualcaffeine.io
 
 @echo off
 setlocal
