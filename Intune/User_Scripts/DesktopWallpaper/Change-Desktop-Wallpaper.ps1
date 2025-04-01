46960505061fc34feeb3cb73bce6b556178fcad5
VirtualCaffeineIO <virtualcaffeineio@gmail.com>
2025-04-01 08:19:12 -0700

    Create Change-Desktop-Wallpaper.ps1

---
+++ Intune/User_Scripts/DesktopWallpaper/Change-Desktop-Wallpaper.ps1
@@ -0,0 +1,37 @@
+# Specify the URL of your custom wallpaper
+$wallpaperUrl = 'https://csjdevstorage.blob.core.windows.net/bloatware/DinosaurPark.jpg'
+
+# Specify the local path to the wallpaper folder and file
+$wallpaperFolder = "C:\Wallpaper-files"
+$localWallpaperPath = Join-Path $wallpaperFolder "wallpaper.jpg"
+
+# Check if the folder exists, and create it if not
+if (-not (Test-Path $wallpaperFolder -PathType Container)) {
+    New-Item -Path $wallpaperFolder -ItemType Directory -Force
+}
+
+# Download the wallpaper
+Invoke-WebRequest -Uri $wallpaperUrl -OutFile $localWallpaperPath
+
+# Check if the download was successful
+if (Test-Path $localWallpaperPath -PathType Leaf) {
+    # Set the registry key for the wallpaper
+    $registryPath = 'HKCU:\Control Panel\Desktop'
+    $name = 'Wallpaper'
+    Set-ItemProperty -Path $registryPath -Name $name -Value $localWallpaperPath
+
+    # Refresh the desktop to apply the changes
+    $signature = @"
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+"@
+    $SPI_SETDESKWALLPAPER = 0x0014
+    $UPDATE_INI_FILE = 0x01
+    $SEND_CHANGE = 0x02
+    $null = Add-Type -MemberDefinition $signature -Name WinAPI -Namespace SystemParametersInfoFunctions -PassThru
+    [SystemParametersInfoFunctions.WinAPI]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $localWallpaperPath, $UPDATE_INI_FILE -bor $SEND_CHANGE)
+
+    Write-Host "Wallpaper changed successfully."
+} else {
+    Write-Host "Error: Wallpaper download failed from $wallpaperUrl."
+}
