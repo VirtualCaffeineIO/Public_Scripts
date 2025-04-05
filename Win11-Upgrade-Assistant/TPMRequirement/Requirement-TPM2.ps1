332e68bf7e9bbcdc96bbbddfecea6bfc7ddefdea
Virtual Caffeine IO <virtualcaffeineio@gmail.com>
2025-04-05 10:13:19 -0700

    Add files via upload

--- Win11-Upgrade-Assistant/TPMRequirement/Requirement-TPM2.ps1
+++ Win11-Upgrade-Assistant/TPMRequirement/Requirement-TPM2.ps1
@@ -1,7 +1,7 @@
 $tpmInfo = Get-CimInstance -Namespace root\CIMV2\Security\MicrosoftTpm -ClassName Win32_Tpm -ErrorAction SilentlyContinue
 
 if ($tpmInfo) {
-    $versionList = $tpmInfo.SpecVersion -split ",\s*"
+    $versionList = ($tpmInfo.SpecVersion -split "[,\s]+") | ForEach-Object { $_.Trim() }
     if ($versionList -contains "2.0" -or $versionList -contains "2.1") {
         Write-Host "TPM 2.0 or 2.1 detected"
         exit 0
