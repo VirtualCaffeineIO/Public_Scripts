b69beba191a04cb209563a2b222e7d66bfc4a48d
Virtual Caffeine IO <virtualcaffeineio@gmail.com>
2025-04-07 19:17:15 -0700

    Update WHfB-Disable-Remediation.ps1

--- Intune_Remediations/Disable-WHfB/WHfB-Disable-Remediation.ps1
+++ Intune_Remediations/Disable-WHfB/WHfB-Disable-Remediation.ps1
@@ -6,7 +6,7 @@ If ($TempDir -eq $False) {
 else {
     write-output "Temp Directory Already Exists"
 }
-Invoke-WebRequest -Uri "https://github.com/gnon17/MS-Cloud-Scripts/raw/main/SetACL.exe" -OutFile C:\Temp\SetACL.exe
+Invoke-WebRequest -Uri "https://github.com/VirtualCaffeineIO/Public_Scripts/blob/689b1c78028623a83ed8db35fdf7c2e03283c425/Files/SetACL.exe" -OutFile C:\Temp\SetACL.exe
 C:\Temp\SetACL.exe -on "C:\windows\ServiceProfiles\LocalService\AppData\Local\Microsoft\Ngc" -ot file -actn setowner -ownr "n:administrators"
 C:\Temp\SetACL.exe -on "C:\windows\ServiceProfiles\LocalService\AppData\Local\Microsoft\Ngc" -ot file -actn ace -ace "n:administrators;p:full" -actn rstchldrn -rst DACL
 Remove-Item "C:\windows\ServiceProfiles\LocalService\AppData\Local\Microsoft\Ngc" -recurse -force
